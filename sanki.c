5a8ddb91ada73da3711c1632985af6e03201cea3
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2022-03-14 00:32:00 +0300

    Update sanki.c

--- sanki.c
+++ sanki.c
@@ -4,39 +4,232 @@
 #include <time.h>
 #include <stdlib.h>
 
-
 int main() {
-
+unsigned int cevap = 0;
 printf("*************************************************************\n");
 printf("Spica Suzuki tarafindan gelistirilen Sanki 1.0'a hosgeldiniz!\n");
-printf("*************************************************************\n");
-printf("\n");
-int cevap = 0;
-do {
+printf("*************************************************************\n \n");
+
+
+    do {
 srand(time(0));
-int random = (rand() % 3) + 1;
+int random = (rand() % 41) + 1;
 
 
 
- if (random == 1){
-soru1();
+switch (random) {
+    case 1 :
+    hiragana1();
 if (cevap >=0)
-{cevap++;}}
-
-
-else if (random == 2){
-    soru2();
+{cevap++;}
+break;
+    case 2 :
+    hiragana2();
 if (cevap >=0)
-{cevap++;}}
-
-
-else{soru3();
+{cevap++;}
+break;
+    case 3 :
+    hiragana3();
 if (cevap >=0)
-{cevap++;}}
-}while (cevap != 3);
+{cevap++;}
+break;
+    case 4 :
+    hiragana4();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 5 :
+    hiragana5();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 6 :
+    hiragana6();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 7 :
+    hiragana7();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 8 :
+    hiragana8();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 9 :
+    hiragana9();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 10 :
+    hiragana10();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 11 :
+    hiragana11();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 12 :
+    hiragana12();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 13 :
+    hiragana13();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 14 :
+    hiragana14();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 15 :
+    hiragana15();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 16 :
+    hiragana16();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 17 :
+    hiragana17();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 18 :
+    hiragana18();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 19 :
+    hiragana19();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 20 :
+    hiragana20();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 21 :
+    hiragana21();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 22 :
+    hiragana22();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 23 :
+    hiragana23();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 24 :
+    hiragana24();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 25 :
+    hiragana25();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 26 :
+    hiragana26();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 27 :
+    hiragana27();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 28 :
+    hiragana28();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 29 :
+    hiragana29();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 30 :
+    hiragana30();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 31 :
+    hiragana31();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 32 :
+    hiragana32();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 33 :
+    hiragana33();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 34 :
+    hiragana34();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 35 :
+    hiragana35();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 36 :
+    hiragana36();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 37 :
+    hiragana37();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 38 :
+    hiragana38();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 39 :
+    hiragana39();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 40 :
+    hiragana40();
+if (cevap >=0)
+{cevap++;}
+break;
+    case 41 :
+    hiragana41();
+if (cevap >=0)
+{cevap++;}
+break;
 
-    
- 
+}
+
+}
+
+while (cevap != 4294967294);
+
 
 return 0;
 }
