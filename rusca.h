eae7e501b23ac8ef2581aba347a0d59e6a2a1d02
Spica-Suzuki <76227757+Spica-Suzuki@users.noreply.github.com>
2022-03-28 03:50:40 +0300

    Create rusca.h

---
+++ rusca.h
@@ -0,0 +1,565 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void rus1() {
+
+     char *cevap = "a";
+     char girdi[4];
+    
+        printf("Bu hangi harftir A-a: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+            
+    }
+void rus2() {
+
+     char *cevap = "b";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Б-б: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+            
+    }
+void rus3() {
+
+     char *cevap = "v";
+     char girdi[4];
+    
+        printf("Bu hangi harftir В-в: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+            
+    }
+void rus4() {
+
+     char *cevap = "g";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Г-г: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+       else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus5() {
+
+     char *cevap = "d";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Д-д: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+       else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus6() {
+
+     char *cevap = "ye";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Е-е: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus7() {
+
+     char *cevap = "yo";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ё-ё: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus8() {
+
+     char *cevap = "j";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ж-ж: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus9() {
+
+     char *cevap = "z";
+     char girdi[4];
+    
+        printf("Bu hangi harftir З-з: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus10() {
+
+     char *cevap = "i";
+     char girdi[4];
+    
+        printf("Bu hangi harftir И-и: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus11() {
+
+     char *cevap = "y";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Й-й: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus12() {
+
+     char *cevap = "k";
+     char girdi[4];
+    
+        printf("Bu hangi harftir К-к: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus13() {
+
+     char *cevap = "l";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Л-л: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus14() {
+
+     char *cevap = "n";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Н-н: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus15() {
+
+     char *cevap = "o";
+     char girdi[4];
+    
+        printf("Bu hangi harftir O-o: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus16() {
+
+     char *cevap = "p";
+     char girdi[4];
+    
+        printf("Bu hangi harftir П-п: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus17() {
+
+     char *cevap = "r";
+     char girdi[4];
+    
+        printf("Bu hangi harftir P-p: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus18() {
+
+     char *cevap = "s";
+     char girdi[4];
+    
+        printf("Bu hangi harftir C-c: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus19() {
+
+     char *cevap = "t";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Т-т: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus20() {
+
+     char *cevap = "u";
+     char girdi[4];
+    
+        printf("Bu hangi harftir У-у: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus21() {
+
+     char *cevap = "f";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ф-ф: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus22() {
+
+     char *cevap = "h";
+     char girdi[4];
+    
+        printf("Bu hangi harftir X-x: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus23() {
+
+     char *cevap = "ts";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ц-ц: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus24() {
+
+     char *cevap = "ç";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ч-ч: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus25() {
+
+     char *cevap = "ş";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ш-ш: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus26() {
+
+     char *cevap = "şç";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Щ-щ: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus27() {
+
+     char *cevap = "sert";
+     char girdi[8];
+    
+        printf("Bu hangi harftir ъ: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus28() {
+
+     char *cevap = "ı";
+     char girdi[4];
+    
+        printf("Bu hangi harftir ы: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus29() {
+
+     char *cevap = "yumuşak";
+     char girdi[8];
+    
+        printf("Bu hangi harftir ь: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus30() {
+
+     char *cevap = "e";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Э-э: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus31() {
+
+     char *cevap = "yu";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Ю-ю: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus32() {
+
+     char *cevap = "ya";
+     char girdi[4];
+    
+        printf("Bu hangi harftir Я-я: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
+void rus33() {
+
+     char *cevap = "m";
+     char girdi[4];
+    
+        printf("Bu hangi harftir М-м: ");
+        scanf("%s", girdi);
+        char ctrl = strcmp(girdi, cevap);
+    
+        printf("Verilen yanıt %s. \n", girdi);
+        
+            if (ctrl == 0 ){
+            printf("Doğru cevap \n");
+        }
+        else { printf("Yanlış cevap, doğru cevap %s olacaktı \n", cevap); }
+        
+    }
