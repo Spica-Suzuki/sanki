199459502b71795f0687ab3f5bb2e2566850496a
Supika <76227757+Spica-Suzuki@users.noreply.github.com>
2024-12-30 05:00:23 +0300

    Update sanki.c

--- sanki.c
+++ sanki.c
@@ -7,27 +7,33 @@
 #include "renk.h"
 
 #define PROGRAM_NAME "sanki"
-
-#define AUTHORS proper_name ("Spica 'Mai' Suzuki")
+#define AUTHORS proper_name ("Spica 'Ceren' Suzuki")
 
 int main() {
     printf("%s······························································\n",YESIL);
     printf("%sSpica Suzuki tarafından geliştirilen Sanki 1.6'ya hoşgeldiniz!\n",MAVI);
     printf("%s······························································\n\n%s",YESIL, NORMAL);
+
     int x;
-    printf("Başlamak için lütfen bir mod seçiniz:  \n\n%s 1-Hiragana\t 2-Kiril%s \n\nSeç: ",SARI, NORMAL);
-    scanf("%d", &x);
+while(1){
+    printf("Başlamak için lütfen bir mod seçiniz:  \n\n%s0-Sankiden çık\t 1-Hiragana\t 2-Kiril%s \n\nSeç: ",SARI, NORMAL);
+    scanf(" %d", &x);
+
     if (x == 1) {
-        hiragana();
-        }        
-    
+
+            hiragana();
+        }
+
     else if (x == 2) {
         rus();
         }
-           
-        
-    else {
-        puts("çıkış");
+
+
+ //   else if (x == 5) {
+
+   //     TERMINATE();
     }
+//      else{}
+
     return 0;
 }
